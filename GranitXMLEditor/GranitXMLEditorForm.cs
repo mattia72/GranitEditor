4a772cef7f9e846035d9fe004a2f55b9278adeb0
mattia72 <mattia72@gmail.com>
2016-12-16 15:38:16 +0100

    refactor

---
+++ GranitXMLEditor/GranitXMLEditorForm.cs
@@ -0,0 +1,179 @@
+﻿using Be.Timvw.Framework.ComponentModel;
+using GranitXMLEditor.Properties;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace GranitXMLEditor
+{
+    public partial class GranitXMLEditorForm : Form
+    {
+
+        private GranitXmlToObject xmlToObject;
+        private OpenFileDialog openFileDialog1 ;
+
+        public GranitXMLEditorForm()
+        {
+            InitializeComponent();
+            LoadXmlFile("default.xml");
+            if (!string.IsNullOrEmpty(Settings.Default.SortedColumn))
+            {
+                DataGridViewColumn col = FindColumnByHeaderText(Settings.Default.SortedColumn);
+                dataGridView1.Sort(col, Settings.Default.SortOrder == "Ascending" ? ListSortDirection.Ascending : ListSortDirection.Descending);
+                col.HeaderCell.SortGlyphDirection = Settings.Default.SortOrder == "Ascending" ? SortOrder.Ascending : SortOrder.Descending;
+            }
+        }
+
+        private DataGridViewColumn FindColumnByHeaderText(string headerText)
+        {
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                if (col.HeaderText == headerText)
+                {
+                    return col;
+                }
+            }
+            return null;
+        }
+
+        public void LoadXml_button_Click(object sender, EventArgs e)
+        {
+            OpenGranitXmlFile();
+        }
+
+        private void OpenGranitXmlFile()
+        {
+            openFileDialog1 = openFileDialog1 == null ? new OpenFileDialog() : openFileDialog1;
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+            openFileDialog1.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string xmlFilePath = Path.GetFullPath(openFileDialog1.FileName);
+                LoadXmlFile(xmlFilePath);
+            }
+        }
+
+        private void LoadXmlFile(string xmlFilePath)
+        {
+            if (xmlToObject == null)
+                xmlToObject = new GranitXmlToObject(xmlFilePath);
+
+            xmlToObject.LoadObjectFromFile(xmlFilePath);
+            var list = new SortableBindingList<TransactionAdapter>(xmlToObject.HUFTransactionAdapter.Transactions);
+            dataGridView1.DataSource = list;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenGranitXmlFile();
+        }
+
+        private void alignTableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (alignTableToolStripMenuItem.Checked)
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+            else
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader;
+
+            alignTableToolStripMenuItem.Checked = !alignTableToolStripMenuItem.Checked;
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (xmlToObject != null)
+            {
+                var saveDlg = new SaveFileDialog();
+                if (saveDlg.ShowDialog() == DialogResult.OK)
+                {
+                    string xmlFilePath = Path.GetFullPath(saveDlg.FileName);
+                    xmlToObject.SaveToFile(xmlFilePath);
+
+                }
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var aboutBox = new PoorMensAboutBox.AboutBox();
+            aboutBox.ShowDialog();
+        }
+
+        private void dataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+
+        }
+
+        //private DataGridViewCellValidatingEventArgs cellErrorLocation;
+        //private string cellErrorText;
+
+        private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            if (dataGridView1.CurrentCell.Tag == e.FormattedValue)
+            {
+                e.Cancel = true;    //Cancel changes of current cell
+                return;
+            }
+
+            //string headerText = dataGridView1.Columns[e.ColumnIndex].HeaderText;
+
+            //string value = "";
+
+
+            //switch (headerText)
+            //{
+            //    case "Originator":
+            //        value = (string)e.FormattedValue;
+            //        if (value.Length != 16 && value.Length != 24)
+            //        {
+            //            dataGridView1.CurrentCell.ToolTipText = "Invalid Value";
+            //            dataGridView1.BackgroundColor = System.Drawing.Color.LightPink;
+            //            e.Cancel = true;
+            //        }
+            //        break;
+            //    default:
+            //        e.Cancel = false;
+            //        break;
+            //}
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            //dataGridView1.Rows[e.RowIndex].ErrorText = String.Empty;
+            //cellErrorLocation = null;
+            //cellErrorText = null;
+            //dataGridView1.BackgroundColor = BackColor;
+        }
+
+        //private void AnnotateCell(string errorMessage, DataGridViewCellValidatingEventArgs editEvent)
+        //{
+        //    cellErrorLocation = editEvent;
+        //    cellErrorText = errorMessage;
+        //}
+
+        private void dataGridView1_CellErrorTextNeeded(object sender, DataGridViewCellErrorTextNeededEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_RowErrorTextNeeded(object sender, DataGridViewRowErrorTextNeededEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_Sorted(object sender, EventArgs e)
+        {
+            var sortedCol = dataGridView1.SortedColumn;
+            var sortDirection = dataGridView1.SortOrder;
+            string sortedField = sortedCol.HeaderText;
+            Settings.Default.SortedColumn = sortedField;
+            Settings.Default.SortOrder = sortDirection.ToString();
+            Settings.Default.Save();
+
+            xmlToObject.GranitXDocument.
+        }
+    }
+}
---
+++ GranitXMLEditor/GranitXmlToObjectBinder.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using GranitXMLEditor;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Linq;
+using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace GranitXMLEditor
+{
+    internal class GranitXmlToObjectBinder
+    {
+        internal HUFTransactions HUFTransactions { get; private set; }
+        internal HUFTransactionAdapter HUFTransactionAdapter { get; private set; }
+        internal XDocument GranitXDocument { get; private set; }
+
+        private GranitXmlToObjectBinder()
+        {
+            GranitXDocument = new XDocument();
+        }
+
+        public GranitXmlToObjectBinder(HUFTransactions hufTransactions) : this()
+        {
+            HUFTransactions = hufTransactions;
+            HUFTransactionAdapter = new HUFTransactionAdapter(HUFTransactions, GranitXDocument);
+            UpdateGranitXDocument();
+        }
+
+        public GranitXmlToObjectBinder(string xmlFilePath) : this()
+        {
+            LoadObjectFromFile(xmlFilePath);
+            UpdateGranitXDocument();
+            HUFTransactionAdapter = new HUFTransactionAdapter(HUFTransactions, GranitXDocument);
+        }
+
+        public void UpdateGranitXDocument()
+        {
+            XDocument xml = new XDocument();
+            using (var writer = xml.CreateWriter())
+            {
+                var ser = new XmlSerializer(HUFTransactions.GetType());
+                ser.Serialize(writer, HUFTransactions);
+            }
+
+            if (GranitXDocument.Elements().Count() == 0)
+                GranitXDocument = xml;
+            else
+            {
+                // merge xmls
+                //GranitXmlDoc.
+            }
+
+
+        }
+
+        public void LoadObjectFromFile(string xmlFilePath)
+        {
+            var ser = new XmlSerializer(typeof(HUFTransactions));
+            var xml = XDocument.Load(xmlFilePath);
+            HUFTransactions = (HUFTransactions)ser.Deserialize(xml.CreateReader());
+        }
+
+        public void SaveToFile(string xmlFilePath)
+        {
+            GranitXDocument.Save(xmlFilePath);
+        }
+
+        public void Sort(string columnText )
+        {
+            var sorted = GranitXDocument.Descendants(Constants.Transaction).OrderBy(x => x.Element("Amount").Value);
+        }
+    }
+}
