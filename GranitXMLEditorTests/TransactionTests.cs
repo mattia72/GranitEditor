c4b8e4031f1b42eae5a8efbbe949e4ab685d8577
mattia72 <mattia72@gmail.com>
2016-12-16 14:44:24 +0100

    New Tests

--- GranitXMLEditorTests/TransactionTests.cs
+++ GranitXMLEditorTests/TransactionTests.cs
@@ -67,6 +67,40 @@ namespace GranitXMLEditor.Tests
             //assert
             Assert.AreNotEqual( t1.CompareTo(t2), 0 );
         }
+
+        [TestMethod()]
+        public void CompareTo_AmountCurrency_NotEqual_Test()
+        {
+            //arrange
+            var xt1 = XDocument.Parse(transactionXml);
+            var transactionXml2 = transactionXml.Replace("HUF", "EUR");
+            var xt2 = XDocument.Parse(transactionXml2);
+
+            var ser = new XmlSerializer(typeof(Transaction));
+            Transaction t1 = (Transaction)ser.Deserialize(xt1.CreateReader());
+            Transaction t2 = (Transaction)ser.Deserialize(xt2.CreateReader());
+            //act
+            //assert
+            Assert.AreNotEqual( t1.CompareTo(t2), 0 );
+        }
+
+        [TestMethod()]
+        public void CompareTo_Amount_NotEqual_Test()
+        {
+            //arrange
+            var xt1 = XDocument.Parse(transactionXml);
+            var transactionXml2 = transactionXml.Replace("1000.00", "1111.11");
+            var xt2 = XDocument.Parse(transactionXml2);
+
+            var ser = new XmlSerializer(typeof(Transaction));
+            Transaction t1 = (Transaction)ser.Deserialize(xt1.CreateReader());
+            Transaction t2 = (Transaction)ser.Deserialize(xt2.CreateReader());
+            //act
+            //assert
+            Assert.AreNotEqual( t1.CompareTo(t2), 0 );
+            Assert.AreEqual( t1.CompareTo(t2), "1000.00".CompareTo("1111.11"));
+        }
+
         private const string transactionXml = @"  
                            <Transaction>
                             <Originator> <Account> <AccountNumber>111111112222222233333333</AccountNumber> </Account> </Originator>
