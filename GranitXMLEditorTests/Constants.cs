6a810990f51de7db5eda7bacf20b64be5816a4ac
mattia72 <mattia72@gmail.com>
2017-01-01 22:21:29 +0100

    TransactionAdapterTest

---
+++ GranitXMLEditorTests/Constants.cs
@@ -0,0 +1,33 @@
+﻿using GranitXMLEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GranitXMLEditorTests
+{
+  public class TestConstants
+  {
+    public static string TransactionXml = @"  
+      <Transaction>
+       <Originator> <Account> <AccountNumber>111111112222222233333333</AccountNumber> </Account> </Originator>
+       <Beneficiary> <Name>Gipsz Jakab</Name> <Account> <AccountNumber>333333334444444455555555</AccountNumber> </Account> </Beneficiary>
+       <Amount Currency = ""HUF"" >1000.00</Amount>
+       <RequestedExecutionDate>2016-12-02</RequestedExecutionDate>
+       <RemittanceInfo> <Text>Utólagos elszámolásra</Text> </RemittanceInfo>
+      </Transaction>
+";
+    public static string HUFTransactionXml = @"
+<HUFTransactions>
+      <Transaction id=""1"" is_active=""true"">
+       <Originator> <Account> <AccountNumber>111111112222222233333333</AccountNumber> </Account> </Originator>
+       <Beneficiary> <Name>Gipsz Jakab</Name> <Account> <AccountNumber>333333334444444455555555</AccountNumber> </Account> </Beneficiary>
+       <Amount Currency = ""HUF"" >1000.00</Amount>
+       <RequestedExecutionDate>2016-12-02</RequestedExecutionDate>
+       <RemittanceInfo> <Text>Utólagos elszámolásra</Text> </RemittanceInfo>
+      </Transaction>
+</HUFTransactions>
+";
+  }
+}
---
+++ GranitXMLEditorTests/TransactionAdapterTests.cs
@@ -0,0 +1,32 @@
+﻿using GranitXMLEditor;
+using GranitXMLEditorTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace GranitXMLEditor.Tests
+{
+  [TestClass()]
+  public class TransactionAdapterTests
+  {
+    [TestMethod()]
+    public void UpdateGranitXDocumentTest()
+    {
+      var xdoc = XDocument.Parse(TestConstants.HUFTransactionXml);
+      XmlRootAttribute xRoot = new XmlRootAttribute();
+      xRoot.ElementName = Constants.HUFTransactions;
+      // xRoot.Namespace = "http://www.cpandl.com";
+      xRoot.IsNullable = true;
+      var ser = new XmlSerializer(typeof(HUFTransactions), xRoot);
+      HUFTransactions t = (HUFTransactions)ser.Deserialize(xdoc.CreateReader());
+      var ta = new TransactionAdapter(t.Transaction[0], xdoc);
+
+      //Act
+      ta.UpdateGranitXDocument(Constants.Active, "True");
+
+      Assert.AreEqual(xdoc.Root.Element(Constants.Transaction)
+        .Attribute(Constants.TransactionActiveAttribute).Value, 
+        ta.IsActive.ToString());
+    }
+  }
+}
