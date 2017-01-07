4382ab7ee973cbe0d8be10d3b3c948eebb00da70
mattia72 <mattia72@gmail.com>
2017-01-07 16:53:47 +0100

    Renamed and new class

---
+++ GranitXMLEditor/TransactionXElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace GranitXMLEditor
+{
+  internal class TransactionXElement : XElement
+  {
+
+    public TransactionXElement():
+      base(Constants.Transaction)
+    {
+      ParsedElement = Parse(TransactionXml);
+    }
+
+    public XElement ParsedElement { get; set; }
+
+    private string TransactionXml = @"  
+      <Transaction>
+       <Originator> <Account> <AccountNumber>000000000000000000000000</AccountNumber> </Account> </Originator>
+       <Beneficiary> <Name></Name> <Account> <AccountNumber>000000000000000000000000</AccountNumber> </Account> </Beneficiary>
+       <Amount Currency = ""HUF"" >1.00</Amount>
+       <RequestedExecutionDate>" + DateTime.Now.ToString(Constants.DateFormat) + @"</RequestedExecutionDate>
+       <RemittanceInfo> <Text>Utólagos elszámolásra</Text> </RemittanceInfo>
+      </Transaction> ";
+  }
+}                                                                     
