e7639407909bde0ab655b9f74e22af24d38e0469
mattia72 <mattia72@gmail.com>
2017-04-04 10:22:31 +0200

    build with appveyor + setup build

--- install-vsix-appveyor.ps1
+++ install-vsix-appveyor.ps1
@@ -1,4 +1,4 @@
   $vsixPath = "$($env:USERPROFILE)\InstallerProjects.vsix"
   (new-object net.webclient).DownloadFile('https://visualstudiogallery.msdn.microsoft.com/fd136a01-a0c8-475f-94dd-240136f86746/file/247375/3/InstallerProjects.vsix', $vsixPath)
-  "`"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
+  "`"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
   & .\install-vsix.cmd
