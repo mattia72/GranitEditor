4e20f0b4623195b719218d731f16da93af12941e
mattia72 <mattia72@gmail.com>
2017-04-04 22:12:44 +0200

    update description

--- GranitXMLEditor/Properties/AssemblyInfo.cs
+++ GranitXMLEditor/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GranitEditor")]
-[assembly: AssemblyDescription("Import xml editor for Granit netBank transactions")]
+[assembly: AssemblyDescription("Xml editor for Granit netBank transactions")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MataSoft Bt.")]
 [assembly: AssemblyProduct("GranitEditor")]
