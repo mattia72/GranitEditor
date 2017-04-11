58eca303a5edd53be184ea75aa45ec12ade1eecb
mattia72 <mattia72@gmail.com>
2017-04-11 08:24:02 +0200

    appveyor update: remove unnecessary install

--- GranitXMLEditor/Properties/AssemblyInfo.cs
+++ GranitXMLEditor/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GranitEditor")]
-[assembly: AssemblyDescription("Xml editor for Granit netBank transactions")]
+[assembly: AssemblyDescription("Editor of Granit netBank transactions")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MataSoft Bt.")]
 [assembly: AssemblyProduct("GranitEditor")]
--- install-vsix-appveyor.ps1
+++ install-vsix-appveyor.ps1
@@ -1,7 +1,7 @@
-  $vsixPath = "$($env:USERPROFILE)\Votive2017.vsix"
-  (new-object net.webclient).DownloadFile('https://marketplace.visualstudio.com/items?itemName=RobMensching.WixToolsetVisualStudio2017Extension', $vsixPath)
-  "`"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
-  & .\install-vsix.cmd
+  #$vsixPath = "$($env:USERPROFILE)\Votive2017.vsix"
+  #(new-object net.webclient).DownloadFile('https://marketplace.visualstudio.com/items?itemName=RobMensching.WixToolsetVisualStudio2017Extension', $vsixPath)
+  #"`"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
+  #& .\install-vsix.cmd
 
   #$vsixPath = "$($env:USERPROFILE)\InstallerProjects.vsix"
   #(new-object net.webclient).DownloadFile('https://visualstudiogallery.msdn.microsoft.com/fd136a01-a0c8-475f-94dd-240136f86746/file/247375/3/InstallerProjects.vsix', $vsixPath)
