208964e3465fc2208095a44e6daee444060b84f4
mattia72 <mattiassicha@gmail.com>
2022-05-11 21:40:43 +0200

    1.4.2.0-beta

--- GranitEditor/Properties/AssemblyInfo.cs
+++ GranitEditor/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyVersion("1.4.*")]
 // This will be the version of the installer also:
-[assembly: AssemblyFileVersion("1.3.3.0")]  
+[assembly: AssemblyFileVersion("1.4.2.0")]  
